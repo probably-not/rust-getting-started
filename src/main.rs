6c8f1954ec1d33e5b34ad31c2c6cd23f87397caf
Coby Benveniste <coby.benveniste@gmail.com>
2023-02-22 09:11:10 +0200

    Derive Debug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     println!("Hello {}", "there");
     println!("Hello {1} {0}", "there", "wow");
-    println!("Hello {name}", name = "there")
+    println!("Hello {name}", name = "there");
+
+    // struct Nope(i32);
+
+    #[derive(Debug)]
+    struct Yup(i32);
+    // println!("Nope: {:?}", Nope(1));
+    println!("Yup: {:?}", Yup(1));
 }

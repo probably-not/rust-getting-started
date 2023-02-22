e6d0ffe4ef4c3144accc2ea88b59417e25a50174
Coby Benveniste <coby.benveniste@gmail.com>
2023-02-22 09:15:11 +0200

    Impl std::fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::fmt;
+
 fn main() {
     println!("Hello {}", "there");
     println!("Hello {1} {0}", "there", "wow");
     println!("Hello {name}", name = "there");
 
-    // struct Nope(i32);
+    struct Nope(i32);
+    impl fmt::Display for Nope {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
 
     #[derive(Debug)]
     struct Yup(i32);
-    // println!("Nope: {:?}", Nope(1));
+    println!("Nope: {}", Nope(1));
     println!("Yup: {:?}", Yup(1));
 }
